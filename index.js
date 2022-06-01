d7ad267f877f1d1686acf108839a7fd6093456e3
Govi <govi18051969@gmail.com>
2022-06-01 19:30:08 +0530

    website main content ready and responsive

---
+++ index.js
@@ -0,0 +1,33 @@
+let imageClassList = ["hero_img","services_card-img","info-img","gallery-image"]; 
+let hamburger = document.getElementById("hamburger")
+
+function changeImages(image_class,currentValue,replaceValue) {
+    let images = document.getElementsByClassName(image_class);
+    console.log(images.length);
+    for (let i = 0; i < images.length; i++) {
+        images[i].src = images[i].src.replace(currentValue, replaceValue);
+    }
+}
+
+if (window.innerWidth <= 900) {
+    imageClassList.forEach((image_class) => {
+        changeImages(image_class,"desktop","mobile");
+    })
+}else{
+    imageClassList.forEach((image_class) => {
+        changeImages(image_class,"mobile","desktop");
+    })
+}
+
+window.onresize = () => {
+    console.log(window.innerWidth);
+    if (window.innerWidth <= 900) {
+        imageClassList.forEach((image_class) => {
+            changeImages(image_class,"desktop","mobile");
+        })
+    }else{
+        imageClassList.forEach((image_class) => {
+            changeImages(image_class,"mobile","desktop");
+        })
+    }
+};
